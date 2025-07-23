5016d05de587d804adafa720074429cb353750cb
Hariharan R <hariharan.r@isteer.com>
2025-07-23 15:04:47 +0530

    * Simple quarkus application with Main method to run from IDE

---
+++ src/main/java/io/lab/imHarish03/MainApp.java
@@ -0,0 +1,10 @@
+package io.lab.imHarish03;
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class MainApp {
+    public static void main(String... args) {
+        Quarkus.run(args);
+    }
+}
